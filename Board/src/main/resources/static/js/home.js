68401293a5fe68e0f614cb2d0ecad8fd26b8970a
kim ji- won <wlfhddl23@gmail.com>
2022-06-21 18:08:13 +0900

    scroll update

--- Board/src/main/resources/static/js/home.js
+++ Board/src/main/resources/static/js/home.js
@@ -7,6 +7,8 @@ window.onload = function() {
 	}, 100);
 
 	menuColor(1);
+
+	selectMenuId = "#menu_a_line_1";
 }
 
 //세로 스크롤 시에는 고정하고 가로 스크롤시에는 움직이는 경우
@@ -17,20 +19,51 @@ $(function() {
 	});
 });
 
-
 var targetOffset_home = $(".homediv").offset().top - 1;
 var targetOffset_program = $(".programdiv").offset().top - 1;
-var targetOffset_trainers = $(".trainersdiv").offset().top - 2;
+var targetOffset_trainers = $(".trainersdiv").offset().top - 1;
+var targetOffset_services = $(".servicesdiv").offset().top - 1;
+
+$(window).resize(function() {
+
+	targetOffset_home = $(".homediv").offset().top - 1;
+	targetOffset_program = $(".programdiv").offset().top - 1;
+	targetOffset_trainers = $(".trainersdiv").offset().top - 1;
+    targetOffset_services = $(".servicesdiv").offset().top - 1;
+});
 
 var $w = $(window).scroll(function() {
 	if ($w.scrollTop() >= targetOffset_home) {
-		menuColor(1);
+		if (!selectMenu) menuColor(1);
+		if (!selectRadio) $("input[name='page']:radio[value='home']").prop('checked', true);
+	}
+	if (Math.floor($w.scrollTop()) == Math.floor(targetOffset_home)+1) {
+		selectMenu = false;
+		selectRadio = false;
 	}
 	if ($w.scrollTop() >= targetOffset_program) {
-		menuColor(2);
+		if (!selectMenu) menuColor(2);
+		if (!selectRadio) $("input[name='page']:radio[value='program']").prop('checked', true);
+	}
+	if (Math.floor($w.scrollTop()) == Math.floor(targetOffset_program)+1) {
+		selectMenu = false;
+		selectRadio = false;
 	}
 	if ($w.scrollTop() >= targetOffset_trainers) {
-		menuColor(3);
+		if (!selectMenu) menuColor(3);
+		if (!selectRadio) $("input[name='page']:radio[value='trainers']").prop('checked', true);
+	}
+	if (Math.floor($w.scrollTop()) == Math.floor(targetOffset_trainers)+1) {
+		selectMenu = false;
+		selectRadio = false;
+	}
+	if ($w.scrollTop() >= targetOffset_services) {
+		if (!selectMenu) menuColor(4);
+		if (!selectRadio) $("input[name='page']:radio[value='services']").prop('checked', true);
+	}
+	if (Math.floor($w.scrollTop()) == Math.floor(targetOffset_services)+1) {
+		selectMenu = false;
+		selectRadio = false;
 	}
 });
 
@@ -41,39 +74,102 @@ function menuColor(value) {
 
 		if (i == value) {
 			$(id).css({ "color": "#006BFF" });
+
+			selectMenuId = id;
 		}
 		else {
 			$(id).css({ "color": "#B4C7E7" });
-
-			$(id).hover(function() {
-				$(this).css("color", "#006BFF");
-			}, function() {
-				$(this).css("color", "#B4C7E7");
-			});
 		}
 	}
 }
 
-$(document).ready(function() {
-	$('#menu_a_line_1').click(function() {
+var selectRadio = false;
 
+$("input:radio[name='page']").click(function() {
+	if ($("input:radio[name='page']:checked").val() == 'home') {
 		var offset = $('.homediv').offset();
 		$('html').animate({ scrollTop: offset.top }, 400);
+	}
 
-	});
-
-	$('#menu_a_line_2').click(function() {
-
+	if ($("input:radio[name='page']:checked").val() == 'program') {
 		var offset = $('.programdiv').offset();
 		$('html').animate({ scrollTop: offset.top }, 400);
+	}
 
-	});
-
-	$('#menu_a_line_3').click(function() {
-
+	if ($("input:radio[name='page']:checked").val() == 'trainers') {
 		var offset = $('.trainersdiv').offset();
 		$('html').animate({ scrollTop: offset.top }, 400);
+	}
 
-	});
+	if ($("input:radio[name='page']:checked").val() == 'services') {
+		var offset = $('.servicesdiv').offset();
+		$('html').animate({ scrollTop: offset.top }, 400);
+	}
 
+	selectRadio = true;
 });
+
+
+var selectMenuId = "";
+var selectMenu = false;
+
+$('#menu_a_line_1').hover(function() {
+	$(this).css("color", "#006BFF");
+}, function() {
+	if (selectMenuId != '#menu_a_line_1')
+		$(this).css("color", "#B4C7E7");
+});
+
+$('#menu_a_line_1').click(function() {
+
+	var offset = $('.homediv').offset();
+	$('html').animate({ scrollTop: offset.top }, 400);
+	menuColor(1);
+	selectMenu = true;
+});
+
+$('#menu_a_line_2').hover(function() {
+	$(this).css("color", "#006BFF");
+}, function() {
+	if (selectMenuId != '#menu_a_line_2')
+		$(this).css("color", "#B4C7E7");
+});
+
+$('#menu_a_line_2').click(function() {
+
+	var offset = $('.programdiv').offset();
+	$('html').animate({ scrollTop: offset.top }, 400);
+	menuColor(2);
+	selectMenu = true;
+});
+
+$('#menu_a_line_3').hover(function() {
+	$(this).css("color", "#006BFF");
+}, function() {
+	if (selectMenuId != '#menu_a_line_3')
+		$(this).css("color", "#B4C7E7");
+});
+
+$('#menu_a_line_3').click(function() {
+
+	var offset = $('.trainersdiv').offset();
+	$('html').animate({ scrollTop: offset.top }, 400);
+	menuColor(3);
+	selectMenu = true;
+});
+
+$('#menu_a_line_4').hover(function() {
+	$(this).css("color", "#006BFF");
+}, function() {
+	if (selectMenuId != '#menu_a_line_4')
+		$(this).css("color", "#B4C7E7");
+});
+
+$('#menu_a_line_4').click(function() {
+
+	var offset = $('.servicesdiv').offset();
+	$('html').animate({ scrollTop: offset.top }, 400);
+	menuColor(4);
+	selectMenu = true;
+});
+
